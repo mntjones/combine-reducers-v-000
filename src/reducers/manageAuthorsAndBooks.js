dfe0d3f4c7c14b3e17a58d4a58a1e98e9ba10018
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-19 18:46:17 +0000

    automatically backed up by learn

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -32,7 +32,17 @@ function authorsReducer(state = [], action) {
     case "REMOVE_AUTHOR":
       idx = state.indexOf(action.id);
       return [...state.slice(0, idx), ...state.slice(idx + 1)];
- 
+      
+    case "ADD_BOOK":
+      let existingAuthor = state.filter(
+        author => author.authorName === action.book.authorName
+      );
+      if (existingAuthor.length > 0) {
+        return state;
+      } else {
+        return [...state, { authorName: action.book.authorName, id: uuid() }];
+      }
+      
     default:
       return state;
   }
